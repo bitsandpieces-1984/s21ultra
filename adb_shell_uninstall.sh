fe39d64493791d2897a096d0dc0deafb05d2f939
Promit <2331308+bitsandpieces-1984@users.noreply.github.com>
2021-03-11 07:10:23 -0800

    Update adb_shell_uninstall.sh

--- adb_shell_uninstall.sh
+++ adb_shell_uninstall.sh
@@ -3,11 +3,11 @@ adb shell pm list packages -f
 
 # bixby packages installed
 pm list packages -f | grep bixby
-package:/system/priv-app/SettingsBixby/SettingsBixby.apk=com.samsung.android.app.settings.bixby
-package:/system/priv-app/SystemUIBixby2/SystemUIBixby2.apk=com.samsung.systemui.bixby2
-package:/data/app/~~9QfdH2129f_H9DKzjgJZVg==/com.samsung.android.bixby.service-42p4RX2JJtNxZQ0k3H8uAA==/base.apk=com.samsung.android.bixby.service
-package:/system/priv-app/BixbyVisionFramework3.5/BixbyVisionFramework3.5.apk=com.samsung.android.bixbyvision.framework
-package:/system/priv-app/BixbyAgentStub/BixbyAgentStub.apk=com.samsung.android.bixby.agent.dummy
+com.samsung.android.app.settings.bixby
+com.samsung.systemui.bixby2
+base.apk=com.samsung.android.bixby.service
+com.samsung.android.bixbyvision.framework
+com.samsung.android.bixby.agent.dummy
 
 # uninstalls run
 adb shell pm uninstall -k --user 0 com.samsung.android.samsungpass
