6fe8202906180e3970ba8eb19a60e4756ca2ab90
Promit <2331308+bitsandpieces-1984@users.noreply.github.com>
2021-03-11 06:54:35 -0800

    Create adb_shell_uninstall.sh
    - adb shell commands to remove basic stuff

---
+++ adb_shell_uninstall.sh
@@ -0,0 +1,3 @@
+adb shell pm uninstall -k --user 0 com.samsung.android.samsungpass
+adb shell pm uninstall -k --user 0 com.samsung.android.samsungpassautofill
+adb shell pm uninstall --user 0 com.google.android.youtube
